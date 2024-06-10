4a5073b1c52a7cc3a9d3d6f44a882d979c4e6422
AhmedMegahedTolba <ahmed.megahed.tolba@gmail.com>
2024-06-10 14:48:10 +0300

    first commit

---
+++ STM32/include/MFPEC_Config.h
@@ -0,0 +1,15 @@
+/*
+ * MFPEC_Config.h
+ *
+ *  Created on: Apr 4, 2024
+ *      Author: Ahmed Megahed
+ */
+
+#ifndef MFPEC_CONFIG_H_
+#define MFPEC_CONFIG_H_
+
+
+
+
+
+#endif /* MFPEC_CONFIG_H_ */
---
+++ STM32/include/SPARSER_Interface.h
@@ -0,0 +1,20 @@
+/*
+ * SPARSER_Interface.h
+ *
+ *  Created on: Apr 4, 2024
+ *      Author: Ahmed Megahed
+ */
+
+#ifndef SPARSER_INTERFACE_H_
+#define SPARSER_INTERFACE_H_
+
+
+u8   PARSE_u8AsciToHex     ( u8 Copy_u8Asci      );
+
+void PARSE_voidParseData   ( u8 * Copy_u8BufData );
+
+void PARSE_voidParseRecord ( u8 * Copy_u8BufData );
+
+
+
+#endif /* SPARSER_INTERFACE_H_ */
